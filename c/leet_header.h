9ca6b61aa9a59b3b8da1a6ac9896c29233336538
Danny Jiang <xinyi904314@yahoo.com.tw>
2016-02-23 09:24:27 +0800

    start of leet code using C

---
+++ c/leet_header.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  leet_header.h
+ *
+ *    Description:  leet code headers.
+ *
+ *        Version:  1.0
+ *        Created:  2016年02月23日 09時19分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#define true    1
+#define false   0
+
+typedef char bool;
---
+++ c/leet_main.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  leet_main.c
+ *
+ *    Description:  leet code entry
+ *
+ *        Version:  1.0
+ *        Created:  2016年02月23日 09時20分05秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "leet_header.h"
+
+#include "n292_num_game.c"
+
+int main(int argc, const char *argv[])
+{
+    printf("%s\n", "Leet Code Entry");
+    return 0;
+}
+
