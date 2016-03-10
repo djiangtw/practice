b7a12cd39933c461e8dc77696b42b01491370aaf
Danny Jiang <xinyi904314@yahoo.com.tw>
2016-03-10 17:24:07 +0800

    solved 104

--- c/src/leet_104_maximum_depth_of_binary_tree.c
+++ c/src/leet_104_maximum_depth_of_binary_tree.c
@@ -12,6 +12,40 @@
 #include "leet_header.h"
 #include "leet_104_maximum_depth_of_binary_tree.h"
 
+ typedef struct TreeNode {
+     int val;
+     struct TreeNode *left;
+     struct TreeNode *right;
+ }TreeNode_t;
+
+static int max_depth(int a, int b)
+{
+    return (a > b) ? a: b;
+}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ *
+ * as root is NULL, return 0
+ * as root has no children, return 1
+ * return 0 when current node is NULL, and plus 1 back when returns from a NULL node.
+ */
+int maxDepth(struct TreeNode* root)
+{
+    int d_left, d_right;
+    if(root == NULL)
+    {
+        return 0;
+    }
+    d_left = maxDepth(root->left);
+    d_right = maxDepth(root->right);
+    return max_depth(d_left + 1, d_right + 1);
+}
+
 int leet_104_maximum_depth_of_binary_tree_test(void)
 {
     printf("%s\n", __FILE__);
