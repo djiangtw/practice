9455c2b887b166de7a6b10947a4e40e6fc74a15c
Danny Jiang <xinyi904314@yahoo.com.tw>
2016-03-11 10:50:12 +0800

    solved 299

---
+++ c/src/leet_299_bulls_and_cows.c
@@ -0,0 +1,54 @@
+/**
+ *     Filename: leet_299_bulls_and_cows.c
+ *  Description: 299. Bulls and Cows
+ *      Version: 1.0
+ *      Created: 09:31:33 03-11-2016
+ *     Revision: none
+ *     Compiler: gcc
+ *       Author: Danny Jiang
+ *          URL: https://leetcode.com/problems/bulls-and-cows/
+ */
+
+#include "leet_header.h"
+#include "leet_299_bulls_and_cows.h"
+#include <string.h>
+
+char* getHint(char* secret, char* guess)
+{
+    char candidates[256] = {0};
+    int i;
+    int s_len;
+    int a = 0, b = 0;
+    char* ret;
+
+    ret = (char*)malloc(sizeof(char) * 4);
+    s_len = strlen(secret);
+
+    for (i = 0; i < s_len; i++) {
+        if(secret[i] == guess[i])
+        {
+            a++;
+        }
+        else
+        {
+            candidates[secret[i]]++;
+        }
+    }
+    for (i = 0; i < s_len; i++) {
+        if(secret[i] != guess[i] && candidates[guess[i]] != 0)
+        {
+            b++;
+            candidates[guess[i]]--;
+        }
+    }
+
+    sprintf(ret, "%dA%dB", a, b);
+    return ret;
+}
+
+int leet_299_bulls_and_cows_test(void)
+{
+    printf("%s\n", __FILE__);
+    return 0;
+}
+
---
+++ c/src/leet_299_bulls_and_cows.h
@@ -0,0 +1,17 @@
+/**
+ *     Filename: leet_299_bulls_and_cows.h
+ *  Description: 299. Bulls and Cows
+ *      Version: 1.0
+ *      Created: 09:31:33 03-11-2016
+ *     Revision: none
+ *     Compiler: gcc
+ *       Author: Danny Jiang
+ *          URL: https://leetcode.com/problems/bulls-and-cows/
+ */
+
+#ifndef __LEET_299_BULLS_AND_COWS_H
+#define __LEET_299_BULLS_AND_COWS_H
+
+extern int leet_299_bulls_and_cows_test(void);
+
+#endif
