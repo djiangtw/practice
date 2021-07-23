bf66ef361e99ff7ad4cf4b82863a7b14810248d6
djiang <danny.jiang@innogritcorp.com>
2021-07-24 00:13:55 +0800

    add code 14 and its simple validation

---
+++ c_2021/array_and_string/code14.longest.common.prefix.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdio.h>
+
+char str1[] = "flower";
+char str2[] = "flow";
+char str3[] = "flight";
+
+char* strr[] = {str1, str2, str3};
+char * longestCommonPrefix(char ** strs, int strsSize){
+    int i, j;
+
+    if(strlen(strs[0]) == 0)
+        return strs[0];
+
+    for( j = 0; j < strlen(strs[0]); j++)
+    {
+        for (i = 1; i < strsSize; i++)
+        {
+            if(j > strlen(strs[i]))
+                return strs[i];
+            if(strs[i][j] != strs[0][j])
+            {
+                strs[0][j] = 0;
+                return strs[0];
+            }
+        }
+    }
+    return strs[0];
+}
+
+void main(void)
+{
+    char* s = longestCommonPrefix(strr, 3);
+    printf("output: %s\n", s);
+}
