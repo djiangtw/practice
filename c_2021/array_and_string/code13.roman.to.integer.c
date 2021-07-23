67bde33ea39fcd26e0360660757ebd1d5768dc59
djiang <danny.jiang@innogritcorp.com>
2021-07-23 20:11:33 +0800

    start coding and review leetcode

---
+++ c_2021/array_and_string/code13.roman.to.integer.c
@@ -0,0 +1,28 @@
+
+int getRomanVal(char c)
+{
+    char val_char[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    int val_int[] = { 1, 5, 10, 50, 100, 500, 1000};
+    for(i = 0; i < sizeof(val_char); i++)
+    {
+        if(c == val_char[i])
+            return val_int[i];
+    }
+    return 0;
+}
+int romanToInt(char * s){
+    int ret = 0;
+    int tmp_a, tmp_b;
+
+    while(*s != 0)
+    {
+        tmp_a = getRomanVal(*s);
+        if(*(s+1) == 0)
+            return ret + tmp_a;
+        else if (tmp_a < getRomanVal(*(s+1)))
+            ret -= tmp_a;
+        else
+            ret += tmp_a;
+    }
+    return ret;
+}
